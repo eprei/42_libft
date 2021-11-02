a5999daf8e463c1676f9d079ac67b75e2118346f
Emiliano Presa custodio <epresa-c@c1r16s4.42lausanne.ch>
2021-11-02 17:56:28 +0100

    add_ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,4 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char *ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+    int i;
+
+    i = 0;
+    while (*haystack)
+
+}
--- libft.h
+++ libft.h
@@ -41,6 +41,6 @@ void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
-char *ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 #endif
