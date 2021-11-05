b89531218c92d6c355c86e721428a0bd4a1d10b5
eprei <93380002+eprei@users.noreply.github.com>
2021-11-05 16:48:59 +0100

    debug some functions

--- ft_striteri.c
+++ ft_striteri.c
@@ -3,11 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: epresa-c <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*   By: Emiliano <Emiliano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 12:45:40 by epresa-c          #+#    #+#             */
-/*   Updated: 2021/11/01 12:45:41 by epresa-c         ###   ########.fr       */
+/*   Updated: 2021/11/05 16:48:59 by Emiliano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
 
+}
